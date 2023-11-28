761a0bfba0c60b51ba66459ecc5cf3501854d60f
Ilariosenpai <ilanabougit2@gmail.com>
2023-11-28 09:39:34 +0100

    exo3

---
+++ Exercice_3/exercice_3.js
@@ -0,0 +1,15 @@
+function vert(){
+
+    let ver = document.getElementById("text").style.color="#008000"
+}
+
+function rouge(){
+
+    let rouj = document.getElementById("text").style.color="#ff0000"
+}
+
+
+function bleu(){
+
+    let ble = document.getElementById("text").style.color="#0000ff"
+}
